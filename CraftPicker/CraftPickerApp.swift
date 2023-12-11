67501b953752299877839755281ea0831fc3d84b
Michal <michal.smialko@gmail.com>
2023-12-11 10:45:41 +0100

    Initial Commit

---
+++ CraftPicker/CraftPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftPickerApp.swift
+//  CraftPicker
+//
+//  Created by Michał Śmiałko on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CraftPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
